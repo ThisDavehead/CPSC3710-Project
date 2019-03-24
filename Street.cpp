a416a95fde06ef411844330c22afdc9fc7f16b33
Jeffrey (Jeff) Deurloo <jdeurloo10@gmail.com>
2019-03-24 15:27:40 -0600

    Street Class draws single instance and is centered on robot

--- Street.cpp
+++ Street.cpp
@@ -1,16 +1,14 @@
 #include "Street.h"
 
 void Street::draw(){
-   //produce them iteratively to form the network
-   //For now, let's make one street then work on generating the network
-   //when we get back here
-
-   glBegin(GL_QUADS);
-   glVertex3f(0,0,0);
-   glVertex3f(width,0,0);
-   glVertex3f(width,0,blockLength);
-   glVertex3f(0,0,blockLength);
-   glEnd();
+    //Draw a single unit of street
+    glTranslatef(width/2,0,0);
+    glBegin(GL_QUADS);
+        glVertex3f(0,0,0);
+        glVertex3f(-width,0,0);
+        glVertex3f(-width,0,blockLength);
+        glVertex3f(0,0,blockLength);
+    glEnd();
 
 }
 
