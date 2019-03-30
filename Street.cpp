148da8787e9281d279f101b5efbc92ca339818c8
jdeurloo10 <48468095+jdeurloo10@users.noreply.github.com>
2019-03-29 20:38:01 -0600

    street fix branch started

--- Street.cpp
+++ Street.cpp
@@ -1,52 +1,46 @@
 #include "Street.h"
-#include <stdlib.h>  // Useful for the following includes.
-#include <stdio.h>
-#include <iostream>
-#include <cstdlib>
 ///Draws the street network
 ///@author Jeff Deurloo
 void Street::draw(){
     //center on robot
-    glTranslatef(width/2,0,0);
+    //glTranslatef(width/2,0,0);
 
-    for(int j=0;j<citySize;j++){
+    for(int j=0;j<citySize+1;j++){
         //draw one column straight to the end
-        for(int i=0;i<citySize;i++){
+
         //draw one unit
         glBegin(GL_QUADS);
             glVertex3f(0,0,0);
             glVertex3f(-width,0,0);
-            glVertex3f(-width,0,blockLength);
-            glVertex3f(0,0,blockLength);
+            glVertex3f(-width,0,citySize*blockLength);
+            glVertex3f(0,0,citySize*blockLength);
         glEnd();
         //Shift forward and draw again
-        glTranslatef(0,0,blockLength);
-        }
-        //once done shift back and over to do it again
-        glTranslatef(-blockLength,0,-(citySize*blockLength));
-    }
+        //glTranslatef(0,0,blockLength);
 
+        //once done shift back and over to do it again
+        glTranslatef(-blockLength,0,0);
+    }
+    glTranslatef(blockLength,0,0);
     //Rotate , shift and draw it again
-    glTranslatef(0,0,-width/2);
+    //glTranslatef(-width/2,0,0);
     glRotatef(90,0,1,0);
-
-        for(int j=0;j<citySize;j++){
+    //glTranslatef(0,0,width/2);
+        for(int j=0;j<citySize+1;j++){
         //draw one column straight to the end
-        for(int i=0;i<citySize;i++){
         //draw one unit
         glBegin(GL_QUADS);
             glVertex3f(0,0,0);
             glVertex3f(-width,0,0);
-            glVertex3f(-width,0,blockLength);
-            glVertex3f(0,0,blockLength);
+            glVertex3f(-width,0,citySize*blockLength);
+            glVertex3f(0,0,citySize*blockLength);
         glEnd();
         //Shift forward and draw again
-        glTranslatef(0,0,blockLength);
-        }
-        //once done shift back and over to do it again
-        glTranslatef(-blockLength,0,-(citySize*blockLength));
-    }
 
+        //once done shift back and over to do it again
+        glTranslatef(-blockLength,0,0);
+    }
+    glTranslatef(0,0,width/2);
 }
 
 /*!
@@ -56,14 +50,14 @@ void Street::draw(){
  @author Jeff Deurloo
 */
 bool Street::checkIfIntersection(const float xpos, const float zpos){
-    printf ("Point %f %f.\n", xpos, zpos);
+     printf ("Point %f %f.\n", xpos, zpos);
     printf ("PointMod %f %f.\n", fmod(xpos,blockLength), fmod(zpos,blockLength));
     bool x = (abs(fmod(xpos,blockLength)) < 5 || abs(fmod(xpos,blockLength)) > blockLength-5) || (fmod(xpos,blockLength) > -5 && fmod(xpos,blockLength) < 5);
     bool z = (abs(fmod(zpos,blockLength)) < 5 || abs(fmod(zpos,blockLength)) > blockLength-5) || (fmod(zpos,blockLength) > -5 && fmod(zpos,blockLength) < 5);
     if(x && z)
         return true;
     else
-        return false;
+return false;
 }
 
 const float Street::getBlockLength(){
