555de4166a8107bee27d4525ce5f8d22631b2ffc
mattd2 <44147975+mattd2@users.noreply.github.com>
2019-04-02 15:19:34 -0600

    Buildings drawn, but not per block

--- Street.cpp
+++ Street.cpp
@@ -35,7 +35,7 @@ void Street::draw(){
 	     glEnd();
 	  }
        }
-       
+
         //Shift forward and draw again
         //glTranslatef(0,0,blockLength);
 
@@ -58,7 +58,7 @@ void Street::draw(){
        glVertex3f(0,0,citySize*blockLength);
        glEnd();
        //Shift forward and draw again
-       
+
        glColor3f(1.0,1.0,0.1);
        for (int i = 0; i < citySize * blockLength + 1; i += 4){
 	  if (i % 8 == 0) // skip every second iteration so it's not a solid line
@@ -71,7 +71,7 @@ void Street::draw(){
 	     glEnd();
 	  }
        }
-       
+
        //once done shift back and over to do it again
        glTranslatef(-blockLength,0,0);
     }
@@ -84,8 +84,57 @@ void Street::draw(){
     glVertex3f(-width,0,width);
     glVertex3f(0,0,width);
     glEnd();
-    
+
 }
+/*void Street::draw(){
+  //center on robot
+    //glTranslatef(width/2,0,0);
+    glTranslatef(width/2,0,-width/2);
+    for(int j=0;j<citySize+1;j++){
+        //draw one column straight to the end
+
+        //draw one unit
+        glBegin(GL_QUADS);
+            glVertex3f(0,0,0);
+            glVertex3f(-width,0,0);
+            glVertex3f(-width,0,citySize*blockLength);
+            glVertex3f(0,0,citySize*blockLength);
+        glEnd();
+        //Shift forward and draw again
+        //glTranslatef(0,0,blockLength);
+
+        //once done shift back and over to do it again
+        glTranslatef(-blockLength,0,0);
+    }
+    glTranslatef(blockLength,0,0);
+    //Rotate , shift and draw it again
+    //glTranslatef(-width/2,0,0);
+    glRotatef(90,0,1,0);
+    //glTranslatef(0,0,width/2);
+        for(int j=0;j<citySize+1;j++){
+        //draw one column straight to the end
+        //draw one unit
+        glBegin(GL_QUADS);
+            glVertex3f(0,0,0);
+            glVertex3f(-width,0,0);
+            glVertex3f(-width,0,citySize*blockLength);
+            glVertex3f(0,0,citySize*blockLength);
+        glEnd();
+        //Shift forward and draw again
+
+        //once done shift back and over to do it again
+        glTranslatef(-blockLength,0,0);
+    }
+    glTranslatef(blockLength,0,0);
+    glTranslatef(0, 0, -width);
+    glBegin(GL_QUADS);
+        glVertex3f(0,0,0);
+        glVertex3f(-width,0,0);
+        glVertex3f(-width,0,width);
+        glVertex3f(0,0,width);
+    glEnd();
+
+}*/
 
 /*!
  Function that check if the robot's position is a at an intersection
